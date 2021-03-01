0a7e00813691ab29eeedf81c2625d38968aae63a
jujimeizuo <61532079+jujimeizuo@users.noreply.github.com>
2021-03-01 16:49:27 +0800

    Update 模板.cpp

--- 模板.cpp
+++ 模板.cpp
@@ -1,35 +1,34 @@
-#pragma GCC optimize(2)
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 
 using namespace std;
 
 typedef long long ll;
 typedef long double ld;
-typedef pair<int, int> pdd;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
 
-#define INF 0x3f3f3f3f
-#define lowbit(x) x & (-x)
+#define endl "\n"
+#define eb emplace_back
 #define mem(a, b) memset(a , b , sizeof(a))
-#define FOR(i, x, n) for(int i = x;i <= n; i++)
 
+const ll INF = 0x3f3f3f3f;
 // const ll mod = 998244353;
 // const ll mod = 1e9 + 7;
-// const double eps = 1e-6;
-// const double PI = acos(-1);
-// const double R = 0.57721566490153286060651209;
+const double eps = 1e-6;
+const double PI = acos(-1);
+const double R = 0.57721566490153286060651209;
 
-
-
-void solve()
-{
-
+void solve() {
+    
 }
 
 signed main() {
     ios_base::sync_with_stdio(false);
-    //cin.tie(nullptr);
-    //cout.tie(nullptr);
-#ifdef FZT_ACM_LOCAL
+    // cin.tie(nullptr);
+    // cout.tie(nullptr);
+#ifdef ACM_LOCAL
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
     signed test_index_for_debug = 1;
@@ -51,20 +50,3 @@ signed main() {
 #endif
     return 0;
 }
-
-/*
- * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
- * │Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│  ┌┐    ┌┐    ┌┐
- * └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘  └┘    └┘    └┘
- * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
- * │~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│ │Num│ / │ * │ - │
- * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
- * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│ │ 7 │ 8 │ 9 │   │
- * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┤ + │
- * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │               │ 4 │ 5 │ 6 │   │
- * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
- * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │     │ 1 │ 2 │ 3 │   │
- * ├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐ ├───┴───┼───┤ E││
- * │ Ctrl│ Win│ Alt│         Space         │ Alt│ Win│Menu│Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │←─┘│
- * └─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
- */
