9e4b48a50704f993cfb6417f7a7f5b49a80f1326
jujimeizuo <61532079+jujimeizuo@users.noreply.github.com>
2021-03-01 16:17:37 +0800

    Update 并查集.cpp

--- 图论/图的连通性/联通块/并查集.cpp
+++ 图论/图的连通性/联通块/并查集.cpp
@@ -1,73 +1,57 @@
 const int maxn = 5e5 + 10;
 
 int n, m, k;
-int pre[maxn], cnt[maxn];
+int f[maxn], cnt[maxn];
 
-int find(int x)
-{
-    if(pre[x] == x)
-        return x;
-    int y = find(pre[x]);
-    pre[x] = y;
-    return y;
+int find(int x) {
+  return f[x] == x ? x : f[x] = find(f[x]);
 }
 
-void merge(int u, int v)
-{
+void merge(int u, int v) {
     int x = find(u);
     int y = find(v);
-    if(x != y)
-    {
-        pre[x] = y;
+    if(x != y) {
+        f[x] = y;
     }
 }
 
-void init(int n)
-{
-    for(int i = 1;i <= n; i++)
-    {
-        pre[i] = i;
+void init(int n) {
+    for(int i = 1;i <= n; i++) {
+        f[i] = i;
         cnt[i] = 0;
     }
 }
 
-void solve()
-{
+void solve() {
     cin >> n >> m;
     init(n);
     int u, v;
-    for(int i = 1;i <= m; i++)
-    {
+    for(int i = 1;i <= m; i++) {
         cin >> u >> v;
         merge(u, v);
     }
     
     int ans = 0;
-    // first
-    for(int i = 1;i <= n; i++)
-    {
+    // first
+    for(int i = 1;i <= n; i++) {
         cnt[find(i)]++;
     }
-    for(int i = 1;i <= n; i++)
-    {
+    for(int i = 1;i <= n; i++) {
         if(cnt[i])
             ans++;
     }
     
     //second
-    for(int i = 1;i <= n; i++)
-    {
+    for(int i = 1;i <= n; i++) {
         int s = find(i);
-        if(cnt[s] == 0)
-        {
+        if(cnt[s] == 0) {
             cnt[s]++;
             ans++;
         }
     }
     
     //third
-    for(int i = 1;i <= n; i++)
-    {
+    for(int i = 1;i <= n; i++) {
         if(pre[i] == i)
             ans++;
     }
