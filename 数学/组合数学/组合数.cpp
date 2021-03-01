118c3cc826571ef150651a3af1d248b023b9125b
jujimeizuo <61532079+jujimeizuo@users.noreply.github.com>
2021-03-01 16:30:08 +0800

    Create 组合数.cpp

---
+++ 数学/组合数学/组合数.cpp
@@ -0,0 +1,25 @@
+typedef long long ll;
+const int N = 1e7 + 10;
+
+ll p; // C(n,m) % p
+
+ll f[N], inv[N], invF[N];
+
+void Init() {
+    f[0] = f[1] = inv[0] = inv[1] = invF[0] = invF[1] = 1;
+    for(int i = 2;i <= p; i++)
+    {
+        f[i] = f[i - 1] * i % p;
+        inv[i] = p - (p / i) * inv[p % i] % p;
+        invF[i] = invF[i - 1] * inv[i] % p;
+    }
+}
+
+ll C(ll m, ll n) {
+    if(m < 0 || n < 0 || n > m)
+        return 0;
+    ll ans = f[m];
+    ans = ans * invF[n] % p;
+    ans = ans * invF[m - n] % p;
+    return ans;
+}
